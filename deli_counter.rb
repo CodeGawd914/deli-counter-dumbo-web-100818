d705c9cd4bcd138634421c8ef4717c580f1de7c8
Travis Desir <travisgdesir31@gmail.com>
2018-09-18 05:04:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number(katz_deli, "name")
   katz_deli.push(name)
   spot = katz_deli(name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line. )"
