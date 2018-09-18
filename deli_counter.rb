5ebaf2903f473be248dc159d373b6bc158bf9d83
Travis Desir <travisgdesir31@gmail.com>
2018-09-18 04:23:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,6 @@
 def line(katz_deli)
   if katz_deli.length == 0
   puts  "The line is currently empty."
-  elsif katz_deli.length >= 1
-  lineTotal = []
-  i = 1 
-  katz_deli.each do |name|
-    lineTotal.push("#{i}. #{name}")
-  end
-  return "The line is currently" lineTotal
   else
-    return katz_deli
-  end
+    
 end
