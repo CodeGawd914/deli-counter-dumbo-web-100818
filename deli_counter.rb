6f53655401d44843287d725dc8a991390d57473a
Travis Desir <travisgdesir31@gmail.com>
2018-09-18 04:37:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.length == 0
   puts  "The line is currently empty."
   else
-    sentence = "The line is currently: "
+    sentence = "The line is currently:"
     katz_deli.each_with_index do |name, index|
     sentence +=" #{index.to_i+1}. #{name}"
   end
