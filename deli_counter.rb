97ef9d1c09db94b323a0c2d3f79025db990ce474
Travis Desir <travisgdesir31@gmail.com>
2018-09-18 03:56:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 def line(katz_deli)
   if katz_deli.length == 0
   puts  "The line is currently empty."
+  elsif katz_deli.length >= 1
+  i = 1
+  sentence = "The line is currently"
+  katz_deli.each do |name|
   else
     return katz_deli
   end
